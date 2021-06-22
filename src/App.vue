21f101b2473fee6d24eb5e62b8a09f8d3017c677
Rukshan <arkruka@gmail.com>
2021-06-23 01:59:03 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NEWS2 />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NEWS2 from './components/News-2-score.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    NEWS2
+  }
+})
+</script>
+
