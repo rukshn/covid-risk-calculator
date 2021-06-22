911578b6d78e8fdfdec81e631acd9d0e9d178283
Rukshan <arkruka@gmail.com>
2021-06-23 01:58:48 +0530

    Initial Commit

---
+++ src/components/News-2-score.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="max-w-7xl mx-auto sm:px-3">
+    <h1 class="text-2xl">COVID Risk Calculator</h1>
+    <h2 class="text-xl text-gray-600">For hospitalized patients</h2>
+    <p class="text-lg text-gray-500">Based on </p>
+
+    <p>Please fill out the following details about the patient</p>
+
+    <div class="parameters space-y-2">
+      <div class="space-y-1">
+        <label class="capitalize font-bold">age</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">age</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">respiratory rate</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">sp O2</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">sp O2 if COPD</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">pulse rate</label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+      <div class="space-y-1">
+        <label class="capitalize font-bold">systolic BP/label>
+        <div>
+          <input type="text"
+            class="rounded-md border-2 px-3 py-1 border-gray-400 hover:border-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-offset-purple-500">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
